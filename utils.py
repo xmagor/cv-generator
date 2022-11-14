e97a8456995d3e3a72e61b8b3d0bdb7682ee00cd
Miguel Gomez <mangel-95@hotmail.com>
2022-11-14 13:19:40 -0500

    feat: rml jinja2 template cv-one-column

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def now(format='%d/%m/%Y'):
     return datetime.now().strftime(format)
 
-def datetime_convert(date_string, current_format="%d/%m/%Y", new_format="%b, %Y"):
+def datetime_convert(date_string, current_format='%d/%m/%Y', new_format='%b, %Y'):
 
     current_date =  datetime(*(time.strptime(date_string, current_format)[0:6]))
     return current_date.strftime(new_format)
