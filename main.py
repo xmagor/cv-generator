92503274afa657a68fe113a1f8bee0b2311ebaf1
Miguel Gomez <mangel-95@hotmail.com>
2022-11-14 12:14:56 -0500

    feat: input data json implemented!

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+import argparse
+import json
 from z3c.rml import rml2pdf
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 DEFAULT_FILENAME = 'sample.pdf'
 DEFAULT_TEMPLATE = 'one-column-default.xml'
+DEFAULT_DATA = 'data'
 TEMPLATES_DIR = 'templates'
 OUTPUT_DIR = 'output'
+
 
 env = Environment(
     loader=FileSystemLoader(TEMPLATES_DIR),
     autoescape=select_autoescape()
 )
+
+
 template = env.get_template(DEFAULT_TEMPLATE)
 
-def main(filename):
+def openJsonData(dir_name):
+
+    with open(f'{DEFAULT_DATA}/{dir_name}/data.json', 'r') as f:
+        data = json.load(f)
+
+    content = data['content']
+    settings = data['settings']
+    return content, settings
+
+
+
+def main(dir_name, filename):
 
     path_to_save = f'{OUTPUT_DIR}/{filename}'
 
-    xml_data = template.render()
+    content, settings = openJsonData(dir_name)
+
+    xml_data = template.render(dir_name=dir_name, content=content, settings=settings)
 
     io_data = rml2pdf.parseString(xml_data, removeEncodingLine=True, filename=filename)
 
@@ -28,5 +47,12 @@ def main(filename):
 
 if __name__ == '__main__':
 
-    filename = DEFAULT_FILENAME
-    main(filename)
+    parser = argparse.ArgumentParser(prog='cv-generator')
+
+    parser.add_argument('input_data',
+        help='Directoy name inside ./data/ with data requeried by the template')
+    parser.add_argument('filename_out', nargs='?', default=DEFAULT_FILENAME,
+        help='PDF Filename that will be stored inside ./output/')
+
+    args = parser.parse_args()
+    main(args.input_data, args.filename_out)
